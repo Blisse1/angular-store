1d2a282a6fd96cf95da3785f463208ce56b0813a
bliss <107047190+Blisse1@users.noreply.github.com>
2024-04-29 09:16:18 -0500

    add product list

---
+++ src/app/product-list/product-list.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { products } from '../products';
+import { CommonModule} from '@angular/common';
+import { ProductAlertsComponent } from '../product-alerts/product-alerts.component';
+
+@Component({
+  selector: 'app-product-list',
+  standalone: true,
+  imports: [CommonModule, ProductAlertsComponent],
+  templateUrl: './product-list.component.html',
+  styleUrl: './product-list.component.css'
+})
+export class ProductListComponent {
+  products = [...products];
+
+  share(){
+    window.alert("The product has been shared!");
+  }
+  onNotify(){
+    window.alert("You will be notified when the product goes on sale");
+  }
+}
